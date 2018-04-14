d1b5e1e29027544c999ec80f7ab331f77045e5b0
BLaurenB <blbillington1@gmail.com>
2018-04-14 15:03:28 -0600

    initial

---
+++ spec/features/user/user_sees_dashboard_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+describe "As an authenticated User" do
+
+  describe "when I sign in I see my dashboard" do
+
+    I see my first and last name, email, I am prompted to add an image
+
+  end
+
+end 

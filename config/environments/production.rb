95e896054429e67d39d0fd11a7c14a26bf9a09cf
BLaurenB <blbillington1@gmail.com>
2018-04-15 17:36:09 -0600

    ES6 issues with uglifier gem. Precompiled and running again

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -26,7 +26,7 @@ Rails.application.configure do
   # Compress JavaScripts and CSS.
   config.action_mailer.default_url_options = { host: 'lvh.me', port: '3000' }
 
-  config.assets.js_compressor = :uglifier
+  config.assets.js_compressor = Uglifier.new(harmony: true)
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
@@ -63,7 +63,7 @@ Rails.application.configure do
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "pytdating_#{Rails.env}"
   config.action_mailer.perform_caching = true
-  
+
   config.action_mailer.delivery_method = :smtp
   # SMTP settings for gmail
   config.action_mailer.smtp_settings = {

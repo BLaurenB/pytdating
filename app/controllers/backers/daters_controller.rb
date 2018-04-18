125a11b0ddf8227d49b4466b807f070dd324b9dc
BLaurenB <blbillington1@gmail.com>
2018-04-18 03:52:46 -0600

    Personality traits are saved properly and displayed correctly on show pages

---
+++ app/controllers/backers/daters_controller.rb
@@ -0,0 +1,46 @@
+class Backers::DatersController < ApplicationController
+
+  def index
+    @daters = Dater.all
+  end
+
+  def show
+    @dater = Dater.find(params[:id])
+    @dater_backer_id = DaterBacker.find_by(dater_id: @dater.id, backer_id: current_backer.id).id
+    @params = params
+  end
+
+  def edit
+    # binding.pry
+    @dater = Dater.find(params[:id])
+    @dater_backer_id = DaterBacker.where(dater_id: @dater.id, backer_id: current_backer.id).first.id
+    @params = params
+  end
+
+  # def edit
+  #   @dater = current_dater
+  # end
+
+  def update
+    # binding.pry
+    if image_params
+        uploaded_io = params[:dater][:image]
+        File.open(Rails.root.join('public', 'uploads', uploaded_io.original_filename), 'wb') do |file|
+          file.write(uploaded_io.read)
+        end
+      Image.create!(user_id: current_dater.user_id, subject: current_dater.id, file: uploaded_io)
+      redirect_to dater_path(current_dater)
+    else
+    end
+
+    # current_dater.update(dater_params)
+  end
+
+  private
+    def image_params
+      params.require(:dater).permit(:image)
+    end
+
+
+
+end

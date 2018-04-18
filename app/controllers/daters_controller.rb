cf88fd09d624162523de61840eeefe948845ed14
BLaurenB <blbillington1@gmail.com>
2018-04-18 01:03:29 -0600

    Fixed Backer Show page so that all dater sections toggle correctly

--- app/controllers/daters_controller.rb
+++ app/controllers/daters_controller.rb
@@ -7,6 +7,7 @@ class DatersController < ApplicationController
   def show
     @dater = Dater.find(params[:id])
     @dater_backer_id = DaterBacker.where(dater_id: @dater.id, backer_id: @dater.id).first.id
+    @params = params
   end
 
   def edit
